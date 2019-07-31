cd88338886b1552afaaa38fd4f097bf0e2fae661
StConte <53525190+StConte@users.noreply.github.com>
2019-07-31 10:23:25 -0400

    Add files via upload

---
+++ lesionVol.m
@@ -0,0 +1,17 @@
+function lesionVol(fnms)
+
+if ~exist('fnms','var')
+	fnms = spm_select(inf,'.mat','Select image[s] for NaN removal'); 
+end
+for i=1:size(fnms,1)
+    fnm = deblank(fnms(i,:));
+    lesionVolumeSub(fnm);
+end
+
+function lesionVolumeSub(fnm)
+m = load(fnm); 
+if ~isfiled(m, 'lesion'), return; end
+vol = sum(m.lesion.dat(:));
+totVol = numel(m.lesion.dat(:));
+%fprintf('s%\t%d\t%d\n', fnm, vol, totVol);
+fprintf('%s\t%d\t%d\n', fnm, vol, totVol);
